01c1efb6e7dd28f3727eaa349d39ee41666813ff
Mallikarjun289 <csdelta289@gmail.com>
2025-01-09 11:51:48 +0530

    fibonacei series

--- core java/Demo019.java
+++ core java/Demo019.java
@@ -1,19 +1,22 @@
-public class Demo019 {
-    
-    public static void fibonacciIterative(int n) {
-        int a = 0, b = 1;
-        System.out.print("Fibonacci Series: ");
-        
-        for (int i = 0; i < n; i++) {
-            System.out.print(a + " ");
-            int nextTerm = a + b;
-            a = b;
-            b = nextTerm;
-        }
-    }
+import java.util.Scanner;
 
+public class Demo019 {
     public static void main(String[] args) {
-        int n = 10;  // Number of terms in the Fibonacci series
-        fibonacciIterative(n);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the number of terms in the Fibonacci series: ");
+        int n = scanner.nextInt();
+
+        int firstTerm = 0, secondTerm = 1;
+        System.out.println("Fibonacci Series up to " + n + " terms:");
+
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(firstTerm + " ");
+
+            // Compute the next term
+            int nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+        }
     }
 }
